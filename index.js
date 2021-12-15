0e8522b742cbe1d995ab94704a9df7b38a5b0eec
Darsh Sikka <67545017+DarshSikka@users.noreply.github.com>
2021-12-15 11:14:30 +0530

    add usrs

--- index.js
+++ index.js
@@ -9,12 +9,29 @@ newla.connect(process.env.NEVLA_URI, {}, () => {
   console.log("newle se baat karli hai wo saap khalega");
 });
 const User = require("./User");
+const LvlUser = require("./LvlUser");
 app.get("/lb", async (req, res) => {
   const { sort } = req.query;
-  const nonsort = sort === "Highest" ? "Won" : "Highest";
+  const nonsort = sort === "highest" ? "won" : "highest";
   const usrs = await User.find({}).sort([
     [sort, -1],
     [nonsort, -1],
+  ]);
+  res.send(usrs);
+});
+app.get("/level", async (req, res) => {
+  const { id } = req.query;
+  const usr = await LvlUser.findOne({ idE: id });
+  if (!usr) {
+    return res.send("invalid user id");
+  } else {
+    return res.send(usr);
+  }
+});
+app.get("/leaderboard-users", async (req, res) => {
+  const usrs = await LvlUser.find({}).sort([
+    ["level", -1],
+    ["xp", -1],
   ]);
   res.send(usrs);
 });
